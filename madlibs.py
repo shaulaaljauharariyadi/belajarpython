205385f3abde26aacbddf2ce6b0cf611a420fb59
265_Shaula Aljauhara Riyadi <103361453+shaulaaljauharariyadi@users.noreply.github.com>
2024-05-08 20:43:06 +0700

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,14 @@
+food = input()
+jamie = input()
+adj1 = input()
+bird = input()
+verb1 = input()
+verb2 = input()
+verb3 = input()
+noun1 = input()
+
+story = "It was " + food + " day at school, and " + jamie + + " was super " + adj1 + " for lunch. But when she went outside to eat, a " + bird + " stole her " + food + "! " + jamie
++ " chased the " + bird + " all over school. She " + verb1 + ", " + verb2 + ", and" + verb3 + " through the playground. Then she tripped on her " + noun1 + " and the " + bird + " escaped!, luckily, "
++ jamie + "'s friends were willing to share their " + food + " with her."
+
+print(story)
